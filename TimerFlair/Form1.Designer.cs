7f38f593445cdfbd8e1204328b375ae974b098dd
Rockoman <laroccomarc@gmail.com>
2015-04-07 04:24:30 -0400

    A timer that counts down from 60 seconds.
    A timer that counts down from 60 seconds and will record the previous 5
    'stops'.

---
+++ TimerFlair/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace TimerFlair
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblSeconds = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblResults = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(56, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(341, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PRESS THE BUTTON FIVE TIMES!";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(85, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(258, 148);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "PRESS";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblSeconds
+            // 
+            this.lblSeconds.AutoSize = true;
+            this.lblSeconds.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSeconds.Location = new System.Drawing.Point(195, 234);
+            this.lblSeconds.Name = "lblSeconds";
+            this.lblSeconds.Size = new System.Drawing.Size(36, 25);
+            this.lblSeconds.TabIndex = 2;
+            this.lblSeconds.Text = "60";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 600;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblResults
+            // 
+            this.lblResults.AutoSize = true;
+            this.lblResults.Location = new System.Drawing.Point(82, 312);
+            this.lblResults.Name = "lblResults";
+            this.lblResults.Size = new System.Drawing.Size(0, 13);
+            this.lblResults.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(349, 121);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(73, 36);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 391);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblResults);
+            this.Controls.Add(this.lblSeconds);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Click! Click! Boom!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblSeconds;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblResults;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ TimerFlair/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimerFlair
+{
+    public partial class Form1 : Form
+    {
+
+        string[] u = new string[] {"Click One", "Click Two", "Click Three", "Click Four", "Click Five"};
+        int[] time = new int[] { 0, 0, 0, 0, 0 };
+        int btnCount = 0;
+        int s = 60;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (btnCount == 5)
+            {
+                s = 60;
+                timer1.Start();
+                lblResults.Text = " ";
+                btnCount = 0;
+                
+            }
+            
+            if (lblSeconds.Text == "")
+                lblSeconds.Text = "0";
+                                    
+                s = Convert.ToInt32(lblSeconds.Text);
+                time[btnCount] = s;
+
+                lblResults.Text += u[btnCount] + " stopped the clock at " + time[btnCount] + " seconds\n";
+
+                btnCount += 1;
+            
+            
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            s = s - 1;
+            string ss = Convert.ToString(s);
+            lblSeconds.Text = ss;
+            if (btnCount == 5)
+            {
+                timer1.Stop();
+
+                MessageBox.Show("That's five times!");
+                
+            }
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            s = 60;
+            btnCount = 0;
+            lblResults.Text = " ";
+            lblSeconds.Text = "60";
+            timer1.Stop();
+            timer1.Start();
+        }
+
+
+    }
+}
